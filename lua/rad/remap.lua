56a5346607cb3c8459c318dff17a1e665b5b9502
radford nguyen <=>
2024-03-23 22:53:13 -0500

    addtl remaps

--- lua/rad/remap.lua
+++ lua/rad/remap.lua
@@ -1,2 +1,3 @@
-vim.g.mapleader = ","
-vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
+vim.g.mapleader = ','
+vim.keymap.set('n', '<leader>fv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>bs', vim.cmd.buffers)
