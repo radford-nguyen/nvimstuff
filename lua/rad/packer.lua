59eb56ce299d776d7a60ef1e80a90c5ead00766a
radford nguyen <=>
2024-03-23 22:53:00 -0500

    adds lsp-zero

--- lua/rad/packer.lua
+++ lua/rad/packer.lua
@@ -6,17 +6,17 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.6',
-      -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
 
     use({
-      'rose-pine/neovim',
-      as = 'rose-pine',
-      config = function()
-      vim.cmd('colorscheme rose-pine')
-      end
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
     })
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
@@ -25,4 +25,26 @@ return require('packer').startup(function(use)
     --use('theprimeagen/harpoon')
     --use('mbbill/undotree')
     use('tpope/vim-fugitive')
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+        -- req'd by lsp-zero ---------------------
+            -- LSP support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lua' },
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        ------------------------------------------
+        }
+    }
 end)
