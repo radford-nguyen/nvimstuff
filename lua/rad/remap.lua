236db1c338b3709ae2645e987a16a9346a286501
Radford Nguyen <=>
2024-04-02 14:29:02 -0500

    colors and adds some remaps

--- lua/rad/remap.lua
+++ lua/rad/remap.lua
@@ -1,3 +1,10 @@
 vim.g.mapleader = ','
 vim.keymap.set('n', '<leader>fv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>bs', vim.cmd.buffers)
+
+-- clear highlighted matches
+vim.keymap.set('n', '<leader><space>', vim.cmd.noh, {})
+
+-- format json
+--vim.keymap.set('n', '<leader>j', vim.cmd("'<,'>!python -m json.tool"))
+--vim.keymap.set('v', '<leader>j', vim.cmd("'<,'>!python -m json.tool"))
