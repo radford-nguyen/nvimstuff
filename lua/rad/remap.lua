ba0bd500ba5d2fcc49e1fdbe93cf754b3923b768
Radford Nguyen <radford.nguyen@amd.com>
2025-03-24 18:53:28 -0500

    add remaps for whitespace, expand filename, word wraps

--- lua/rad/remap.lua
+++ lua/rad/remap.lua
@@ -9,3 +9,16 @@ vim.keymap.set('n', '<leader><space>', vim.cmd.noh, {})
 --vim.keymap.set('n', '<leader>j', vim.cmd("'<,'>!python -m json.tool"))
 --vim.keymap.set('v', '<leader>j', vim.cmd("'<,'>!python -m json.tool"))
 
+--nnoremap <leader>W :%s/\s\+$//<cr>:let @/=''<CR>
+--nnoremap <leader>f :let @" = expand("%")<CR>
+
+vim.keymap.set('n', '<leader>W', function()
+	vim.cmd(":%s/\\s\\+$//")
+	vim.cmd.noh()
+end)
+vim.keymap.set('n', '<leader>f', ':let @" = expand("%")<CR>')
+
+-- word-wrapping
+vim.keymap.set('n', '<leader>q', 'ciw\'<c-r>"\'')
+vim.keymap.set('n', '<leader>Q', 'ciw"<c-r>""')
+vim.keymap.set('n', '<leader>p', 'ciw(<c-r>")')
