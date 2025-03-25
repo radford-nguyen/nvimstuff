db2ba183eb92a63a20655f5198eeb15d7109a4c9
Radford Nguyen <radford.nguyen@amd.com>
2025-03-24 19:16:59 -0500

    add remaps to fmt json, put filename in pasta buffet

--- lua/rad/remap.lua
+++ lua/rad/remap.lua
@@ -1,22 +1,31 @@
 vim.g.mapleader = ','
+
+-- open netrw
 vim.keymap.set('n', '<leader>fv', vim.cmd.Ex)
+
+-- show buffs
 vim.keymap.set('n', '<leader>bs', vim.cmd.buffers)
 
 -- clear highlighted matches
 vim.keymap.set('n', '<leader><space>', vim.cmd.noh, {})
 
 -- format json
---vim.keymap.set('n', '<leader>j', vim.cmd("'<,'>!python -m json.tool"))
---vim.keymap.set('v', '<leader>j', vim.cmd("'<,'>!python -m json.tool"))
+fmtjson = function()
+	vim.cmd("'<,'>!python -m json.tool")
+end
+vim.keymap.set('n', '<leader>j', fmtjson)
+vim.keymap.set('v', '<leader>j', fmtjson)
 
---nnoremap <leader>W :%s/\s\+$//<cr>:let @/=''<CR>
---nnoremap <leader>f :let @" = expand("%")<CR>
-
+-- remove trailing WS
 vim.keymap.set('n', '<leader>W', function()
 	vim.cmd(":%s/\\s\\+$//")
 	vim.cmd.noh()
 end)
-vim.keymap.set('n', '<leader>f', ':let @" = expand("%")<CR>')
+
+-- puts the current filename in the pasta buffet
+vim.keymap.set('n', '<leader>fn', ':let @" = expand("%")<CR>')
+-- puts the current fq-filename in the pasta buffet
+vim.keymap.set('n', '<leader>ffn', ':let @" = expand("%:p")<CR>')
 
 -- word-wrapping
 vim.keymap.set('n', '<leader>q', 'ciw\'<c-r>"\'')
